68bda6c38675a8fc1faeaf47a44b9b0c0d5f0d68
Mary Claire Elliott <spitsfire@Marys-MacBook-Pro.local>
2020-03-05 16:57:57 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import PersonList from './components/PersonList';
+
+const list = [
+  {key: 1, firstName: "Claire", lastName: "Elliott", age: 30, hairColor: "blond"},
+  {key: 2, firstName: "Tom", lastName: "Servo", age: 30, hairColor: "bald"},
+  {key: 3, firstName: "Crow T.", lastName: "Robot", age: 30, hairColor: "gold"},
+  {key: 4, firstName: "Joel", lastName: "Robinson", age: 30, hairColor: "brown"},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="text-6xl text-center">Prop It Up!</h1>
+        <p className="text-center font-light">Featuring Tailwind CSS</p>
+        <div className="w-full flex justify-center">
+          <PersonList list={list} />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export {App, list};
